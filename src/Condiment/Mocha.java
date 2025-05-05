6709138a68a6c3f460ecc41b63dedf784dbb1644
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-05-04 21:04:22 -0400

    added Mocha

---
+++ src/Condiment/Mocha.java
@@ -0,0 +1,17 @@
+package Condiment;
+
+import Beverage.Beverage;
+
+public class Mocha extends CondimentDecorator{
+    public Mocha(Beverage beverage){
+        this.beverage = beverage;
+    }
+    public String getDescription(){
+        return beverage.getDescription() + ", Mocha";
+    }
+
+    @Override
+    public double cost() {
+        return beverage.cost() + .20;
+    }
+}
