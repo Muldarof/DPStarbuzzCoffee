63f6592cab1d40598be4a5cb589b54414b277477
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-05-04 20:47:37 -0400

    added condiment decorator class

--- src/Condiment/CondimentDecorator.java
+++ src/Condiment/CondimentDecorator.java
@@ -1,4 +1,9 @@
 package Condiment;
 
-public class CondimentDecorator {
+import Beverage.Beverage;
+
+public abstract class CondimentDecorator extends Beverage {
+    Beverage beverage;
+    public abstract String getDescription();
 }
+// we extend the beverage class here because we need to be interchangeable with beverages
