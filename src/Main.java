e81b78d6891cf67fd5fc43f44454854a151da724
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-05-04 21:37:43 -0400

    updated main with second test case

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 import Beverage.Beverage;
 import Beverage.Espresso;
+import Beverage.DarkRoast;
+import Beverage.HouseBlend;
+import Beverage.Decaf;
+
+import Condiment.Mocha;
+import Condiment.SteamedMilk;
+import Condiment.Soy;
+import Condiment.Whip;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,5 +17,12 @@ public class Main {
 
         Beverage beverage = new Espresso();
         System.out.println(beverage.getDescription() + " $" + beverage.cost());
+
+        Beverage beverage2 = new DarkRoast();
+        beverage2 = new Mocha(beverage2);
+        beverage2 = new Mocha(beverage2);
+        beverage2 = new Whip(beverage2);
+        System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
+
     }
 }
