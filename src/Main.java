179a3164bad84bc9c6eb103bff5dc427e6eb8e04
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-05-04 21:32:34 -0400

    updated main test cases

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import Beverage.Beverage;
+import Beverage.Espresso;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        
+        Beverage beverage = new Espresso();
+        System.out.println(beverage.getDescription() + "$" + beverage.cost());
     }
 }
